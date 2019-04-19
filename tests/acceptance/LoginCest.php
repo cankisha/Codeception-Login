9a4a27b26ffb3df7eb393eab25b95940170e8304
cankisha <canko_canev@abv.bg>
2019-04-19 22:55:09 +0300

    Testing dir.bg login functionality

--- tests/acceptance/LoginCest.php
+++ tests/acceptance/LoginCest.php
@@ -1,15 +1,17 @@
 <?php 
 
-class LoginCest
+class LoginCest 
 {
-    public function _before(AcceptanceTester $I)
+    public function frontpageWorks(AcceptanceTester $I)
     {
+		$I->wantTo ('login into dir.bg');
+        $I->amOnPage('/login');
+        $I->see('Вход');  
+		$I->fillField('email', 'canko_canev@abv.bg');
+        $I->fillField('password', 'mNUNzH5rkM');
+        $I->click('Влез');
+		$I->seeInCurrentUrl('/profile');
+		$I->see('Изход');
     }
-
-    // tests
-    public function tryToTest(AcceptanceTester $I)
-    {
-		$I->amOnPage('/dsadasas');
-		$I->seeElement('.notice');
-    }
+	
 }
